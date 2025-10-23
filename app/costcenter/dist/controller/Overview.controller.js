e31136274634457acba5cd98216a91f7e79d22d5
Arjun N <arjn@deloitte.com>
2025-10-23 17:42:23 +0000

    Created and Deployed

---
+++ app/costcenter/dist/controller/Overview.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],e=>{"use strict";return e.extend("com.deloitte.mdg.cost.center.costcenter.controller.Overview",{onInit(){}})});
+//# sourceMappingURL=Overview.controller.js.map
